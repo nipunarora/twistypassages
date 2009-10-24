5a748f4a00cc09228c51859a8d10f52a425b9dee
ma.danbo <ma.danbo@6820ff12-bea8-11de-a250-c7f789e21d83>
2009-10-24 19:13:36 +0000

    copied DumbPlayer1 to a new class for our use.

---
+++ TwistyPassages_PPS_project3/src/maze/g3/G3Player.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package maze.g0;
+
+import maze.ui.Move;
+import maze.ui.Player;
+
+/**
+ *
+ * @author Satyajeet
+ */
+public class G3Player implements Player {
+//test comment
+    public Move move(int object_detail, int number_of_objects, int number_of_turns) {
+        
+        Move myMove = new Move(1,0);
+        return myMove;
+
+    }
+
+}
