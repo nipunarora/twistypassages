74207618c7c30fb26522a3bf634922c9211be2b8
ma.danbo <ma.danbo@6820ff12-bea8-11de-a250-c7f789e21d83>
2009-10-25 06:17:02 +0000

    simple player stuff just to see it move around. implemented the logger class from project 1. no interesting data structures or anything until we figure it out.

--- TwistyPassages_PPS_project3/src/maze/g3/G3Player.java
+++ TwistyPassages_PPS_project3/src/maze/g3/G3Player.java
@@ -3,22 +3,93 @@
  * and open the template in the editor.
  */
 
-package maze.g0;
+package maze.g3;
+
+import java.util.HashSet;
+import java.util.Random;
+
+import maze.g3.Logger;
+import maze.g3.Logger.LogLevel;
 
 import maze.ui.Move;
 import maze.ui.Player;
 
 /**
  *
- * @author Satyajeet
+ * @author Shilpa
+ * @author Nipun
+ * @author Colin
  */
 public class G3Player implements Player {
 //test comment
-    public Move move(int object_detail, int number_of_objects, int number_of_turns) {
-        
-        Move myMove = new Move(1,0);
-        return myMove;
-
+	
+	private boolean gameStart = true;
+	private Logger log = new Logger( LogLevel.DEBUG, this.getClass() );
+	
+	// toggle to systematically explore tunnels
+	private boolean systematicExploration = true;
+	private int whichTunnel = 0;
+	private Random random = new Random();
+	
+	// bag of unique objects that shrinks or grows according to which objects have been dropped
+	private HashSet<Item> bagOfItems = new HashSet();
+	
+	
+	public Move move(int object_detail, int number_of_objects, int number_of_turns) {
+		
+		log.debug("found object: " + object_detail + " num objects: " + number_of_objects + " num turns: " + number_of_turns);
+		
+		if( gameStart ) {
+			initializeBag( number_of_objects );
+			whichTunnel = 0;
+			gameStart = false;
+		}
+		else {
+			whichTunnel = decideOnATunnel();
+		} 
+		
+		Move myMove = new Move( whichTunnel, 0 );
+		return myMove;
     }
+
+	
+	//run the first time to populate the bagOfItems hashset
+	private void initializeBag(int numberOfObjects) {
+		for( int i = 1; i <= numberOfObjects; i++ ) {
+			placeInBag( new Item(i) );
+		}
+		
+	}
+
+
+	//adds to hashset of unique items
+	private void placeInBag( Item item ) {
+		bagOfItems.add( item );
+	}
+
+
+	/**
+	 * basic method for choosing a tunnel
+	 * @return whichTunnel is the number of the tunnel to take
+	 */
+	private int decideOnATunnel() {
+
+		if( systematicExploration ) {
+			
+			if( whichTunnel < 9 ) {
+				whichTunnel++;
+			} 
+			else {
+				systematicExploration = false;
+				whichTunnel = random.nextInt(9);
+			}
+		}
+		else {
+			whichTunnel = random.nextInt(9);
+		}
+
+		log.debug("whichTunnel: "+whichTunnel);
+		return whichTunnel;
+	}
 
 }
---
+++ TwistyPassages_PPS_project3/src/maze/g3/Item.java
@@ -0,0 +1,27 @@
+package maze.g3;
+
+public class Item {
+	
+	private int indexNumber;
+	
+	public Item ( int indexNumber ) {
+		setIndexNumber( indexNumber );
+	}
+
+	
+	/**
+	 * @param indexNumber the indexNumber to set
+	 */
+	public void setIndexNumber(int indexNumber) {
+		this.indexNumber = indexNumber;
+	}
+
+	
+	/**
+	 * @return the indexNumber
+	 */
+	public int getIndexNumber() {
+		return indexNumber;
+	}
+
+}
