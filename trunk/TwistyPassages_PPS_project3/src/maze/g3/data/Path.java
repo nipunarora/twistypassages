fa27c20898bfa04861b7926d86b42b481752eb09
ma.danbo <ma.danbo@6820ff12-bea8-11de-a250-c7f789e21d83>
2009-11-09 03:01:13 +0000

    added a breadth first search, but have not yet tested it on anything.

--- trunk/TwistyPassages_PPS_project3/src/maze/g3/data/Path.java
+++ trunk/TwistyPassages_PPS_project3/src/maze/g3/data/Path.java
@@ -1,6 +1,9 @@
 package maze.g3.data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Vector;
 
 /**
@@ -44,5 +47,76 @@ public class Path {
 			v.add(e);
 			destinationPaths.put(startRoom, v);
 		}
+	}
+	
+	/**
+	 * 
+	 * @param startRoom
+	 * @param targetRoom must be different than startRoom
+	 * @return Vector of edges
+	 */
+	public Vector<Edge> getShortestPath( int startRoom, int targetRoom ) {
+		boolean found = false;
+		//each room connected with its parent edge
+		HashMap<Integer, Edge> parentPath = new HashMap<Integer, Edge>();
+		//a list of vectors of edges l->v->e
+		ArrayList<Vector<Edge>> queueOfVectorsOfEdges = new ArrayList<Vector<Edge>>();
+		ArrayList<Edge> edgesToVisit = new ArrayList<Edge>();
+		HashSet<Integer> visitedRooms = new HashSet<Integer>();
+		//add the vectors of edges to the list of vectors of edges
+		queueOfVectorsOfEdges.add(startPaths.get(startRoom));
+		parentPath.put(startRoom, null);
+		//cycle through the list to look at each vector of edges
+		while( !queueOfVectorsOfEdges.isEmpty()) {
+			//get the vector of edges coming from one room
+			Vector<Edge> v = queueOfVectorsOfEdges.get(0);
+			queueOfVectorsOfEdges.remove(0);
+			//cycle through the vector of edges
+			int vsize = v.size();
+			for (int i=0; i<vsize; i++) {
+				//look at an edge
+				Edge e = v.get(i);
+				if ( ! visitedRooms.contains(e.StartRoom)) {
+					queueOfVectorsOfEdges.add(startPaths.get(e.DestinationRoom));
+					edgesToVisit.add(e);
+					parentPath.put(e.DestinationRoom, e);
+					if ( e.DestinationRoom == targetRoom ) {
+						return constructShortestPath( parentPath, e.DestinationRoom );
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param parentPath
+	 * @param destinationRoom
+	 * @return path (a vector of edges) from the starting point to the target point
+	 */
+	private Vector<Edge> constructShortestPath(HashMap<Integer, Edge> parentPath,
+			int destinationRoom) {
+		// TODO Auto-generated method stub
+		Vector<Edge> path = new Vector<Edge>();
+		Edge parentEdge = parentPath.get(destinationRoom);
+		while ( parentEdge != null ) {
+			path.add( parentPath.get( destinationRoom ));
+			parentEdge = parentPath.get( destinationRoom );
+		}
+		return( flipPath(path) );
+	}
+
+	/**
+	 * @param path (a vector of edges) from target point to starting point
+	 * @return path (a vector of edges) from the starting point to the target point
+	 */
+	
+	private Vector<Edge> flipPath(Vector<Edge> path) {
+		// TODO Auto-generated method stub
+		Vector<Edge> flippedPath = null;
+		for (int i=path.size(); i>=0; i--) {
+			flippedPath.add(path.get(i));
+		}
+		return flippedPath;
 	}
 }
