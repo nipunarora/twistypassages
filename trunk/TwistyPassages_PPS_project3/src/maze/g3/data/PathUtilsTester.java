66141608032dbcf70892ab0f9e2fa3fd14a9bbd0
ma.danbo <ma.danbo@6820ff12-bea8-11de-a250-c7f789e21d83>
2009-11-12 02:55:07 +0000

    unit testing for Path class

--- trunk/TwistyPassages_PPS_project3/src/maze/g3/data/PathUtilsTester.java
+++ trunk/TwistyPassages_PPS_project3/src/maze/g3/data/PathUtilsTester.java
@@ -12,7 +12,7 @@ public class PathUtilsTester {
 
 		Path path = new Path();
 		path.addPath(3, 5, 3);
-		path.addPath(3,6,5);
+		path.addPath(3, 6, 5);
 		path.addPath(3, 4, 2);
 		path.addPath(2, 3, 7);
 		path.addPath(2, 4, 3);
@@ -21,21 +21,38 @@ public class PathUtilsTester {
 		path.addPath(7, 6, 5);
 		path.addPath(7, 5, 5);
 		
-		Vector<Edge> shortestPath = path.getShortestPath(1, 5);
 		
 		System.out.println( "BFS" );
+		Vector<Edge> shortestPath = path.getShortestPath(1, 5);
 		for( Edge e: shortestPath ) {
 			System.out.println( e.StartRoom + "->" + e.door + "->" + e.DestinationRoom );
 		}
 		
-		Vector<Edge> v = new Vector<Edge>();
-		v.add(new Edge(1,0,2));
-		v.add(new Edge(1,1,3));
-		v.add(new Edge(2,4,3));
-		v.add(new Edge(2,3,7));
-		v.add(new Edge(3,4,2));
-		v.add(new Edge(3,6,5));
-		v.add(new Edge(3, 5, 3));
+		//MERGE
+		System.out.println( "MERGE");
+//		path.displayPaths();
+//		Vector<Integer> matchedRooms = new Vector<Integer>();
+//		matchedRooms.add(3);
+//		matchedRooms.add(7);
+//		path.mergeRooms(matchedRooms);
+//		for ( int r: matchedRooms){
+//			System.out.println("match "+r);
+//		}
+//		path.displayPaths();
+//		
+//		path.addPath(3, 5, 3);
+//		path.addPath(3,6,5);
+//		path.addPath(3, 4, 2);
+//		path.addPath(2, 3, 7);
+//		path.addPath(2, 4, 3);
+//		path.addPath(1, 1, 3);
+//		path.addPath(1, 0, 2);
+//		path.addPath(7, 6, 5);
+//		path.addPath(7, 5, 5);
+
+		path.displayPaths();
+		path.mergeAllSimilarRooms();
+		path.displayPaths();
 		
 	}
 
