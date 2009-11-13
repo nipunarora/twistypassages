f1ed161d5ab45d06b34d5cab48b2bd78c595be19
ma.danbo <ma.danbo@6820ff12-bea8-11de-a250-c7f789e21d83>
2009-11-13 05:54:30 +0000

    fixed merge to allow for empty destpaths map and did something else to bfs

--- TwistyPassages_PPS_project3/src/maze/g3/data/PathUtilsTester.java
+++ TwistyPassages_PPS_project3/src/maze/g3/data/PathUtilsTester.java
@@ -20,7 +20,7 @@ public class PathUtilsTester {
 		path.addPath(1, 0, 2);
 		path.addPath(7, 6, 5);
 		path.addPath(7, 5, 5);
-		
+//		path.displayPaths();
 		
 		System.out.println( "BFS" );
 		Vector<Edge> shortestPath = path.getShortestPath(1, 5);
@@ -30,29 +30,21 @@ public class PathUtilsTester {
 		
 		//MERGE
 		System.out.println( "MERGE");
-//		path.displayPaths();
-//		Vector<Integer> matchedRooms = new Vector<Integer>();
-//		matchedRooms.add(3);
-//		matchedRooms.add(7);
+		path.displayPaths();
+		Vector<Integer> matchedRooms = new Vector<Integer>();
+		//matchedRooms.add(3);
+		//matchedRooms.add(7);
 //		path.mergeRooms(matchedRooms);
-//		for ( int r: matchedRooms){
-//			System.out.println("match "+r);
-//		}
-//		path.displayPaths();
-//		
-//		path.addPath(3, 5, 3);
-//		path.addPath(3,6,5);
-//		path.addPath(3, 4, 2);
-//		path.addPath(2, 3, 7);
-//		path.addPath(2, 4, 3);
-//		path.addPath(1, 1, 3);
-//		path.addPath(1, 0, 2);
-//		path.addPath(7, 6, 5);
-//		path.addPath(7, 5, 5);
+		for ( int r: matchedRooms){
+			System.out.println("match "+r);
+		}
+		path.displayPaths();
 
-		path.displayPaths();
-		Vector<Integer> matches = path.mergeAllSimilarRooms(2);
-		path.displayPaths();
+		System.out.println(path.hasMatchingRoom(3, Path.PathsToCheck.BOTH, 3));
+		path.mergeRooms(matchedRooms);
+//		path.displayPaths();
+//		Vector<Integer> matches = path.mergeAllSimilarRooms(2);
+//		path.displayPaths();
 		
 	}
 
